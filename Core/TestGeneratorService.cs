81bc6c13aec15834c85061728cec56a915400e1c
Nikita Hripach <nikita.hripach@gmail.com>
2022-11-08 01:00:32 +0300

    Fix generator service

--- Core/TestGeneratorService.cs
+++ Core/TestGeneratorService.cs
@@ -58,42 +58,87 @@ namespace Core
 				(current, loadDirective) => current.AddUsings(loadDirective));
 
 			var @namespace = SyntaxFactory
-				.NamespaceDeclaration(SyntaxFactory.QualifiedName(SyntaxFactory.IdentifierName("GeneratedNamespace"),
-					SyntaxFactory.IdentifierName(SyntaxFactory.Identifier(SyntaxFactory.TriviaList(), "Tests",
-						SyntaxFactory.TriviaList(SyntaxFactory.CarriageReturnLineFeed)))))
-				.WithNamespaceKeyword(SyntaxFactory.Token(
-					SyntaxFactory.TriviaList(SyntaxFactory.CarriageReturnLineFeed,
-						SyntaxFactory.CarriageReturnLineFeed), SyntaxKind.NamespaceKeyword,
-					SyntaxFactory.TriviaList(SyntaxFactory.Space)))
-				.WithOpenBraceToken(SyntaxFactory.Token(SyntaxFactory.TriviaList(), SyntaxKind.OpenBraceToken,
-					SyntaxFactory.TriviaList(SyntaxFactory.CarriageReturnLineFeed)))
-				.WithCloseBraceToken(SyntaxFactory.Token(SyntaxFactory.TriviaList(SyntaxFactory.CarriageReturnLineFeed),
-					SyntaxKind.CloseBraceToken, SyntaxFactory.TriviaList())).AddMembers(SyntaxFactory
-					.ClassDeclaration(SyntaxFactory.Identifier(SyntaxFactory.TriviaList(),
-						classDeclaration.Identifier.Text + "Test",
-						SyntaxFactory.TriviaList(SyntaxFactory.CarriageReturnLineFeed)))
-					.WithAttributeLists(SyntaxFactory.SingletonList(SyntaxFactory
-						.AttributeList(SyntaxFactory.SingletonSeparatedList(
-							SyntaxFactory.Attribute(SyntaxFactory.IdentifierName("TestFixture"))))
-						.WithOpenBracketToken(SyntaxFactory.Token(
-							SyntaxFactory.TriviaList(SyntaxFactory.CarriageReturnLineFeed,
-								SyntaxFactory.Whitespace(Tab)), SyntaxKind.OpenBracketToken,
-							SyntaxFactory.TriviaList()))
-						.WithCloseBracketToken(SyntaxFactory.Token(SyntaxFactory.TriviaList(),
-							SyntaxKind.CloseBracketToken,
-							SyntaxFactory.TriviaList(SyntaxFactory.CarriageReturnLineFeed)))))
-					.WithModifiers(SyntaxFactory.TokenList(SyntaxFactory.Token(
-						SyntaxFactory.TriviaList(SyntaxFactory.Whitespace(Tab)), SyntaxKind.PublicKeyword,
-						SyntaxFactory.TriviaList(SyntaxFactory.Space))))
-					.WithKeyword(SyntaxFactory.Token(SyntaxFactory.TriviaList(), SyntaxKind.ClassKeyword,
-						SyntaxFactory.TriviaList(SyntaxFactory.Space)))
-					.WithOpenBraceToken(SyntaxFactory.Token(
-						SyntaxFactory.TriviaList(SyntaxFactory.Whitespace(Tab)), SyntaxKind.OpenBraceToken,
-						SyntaxFactory.TriviaList(SyntaxFactory.CarriageReturnLineFeed))).WithCloseBraceToken(
-						SyntaxFactory.Token(SyntaxFactory.TriviaList(new[]
-						{
-							SyntaxFactory.Whitespace(Tab),
-						}), SyntaxKind.CloseBraceToken, SyntaxFactory.TriviaList()))
+				.NamespaceDeclaration(SyntaxFactory
+					.QualifiedName(SyntaxFactory
+						.IdentifierName("GeneratedNamespace"), SyntaxFactory
+						.IdentifierName(SyntaxFactory
+							.Identifier(SyntaxFactory
+								.TriviaList(),
+								"Tests", SyntaxFactory
+								.TriviaList(SyntaxFactory
+									.CarriageReturnLineFeed)))))
+				.WithNamespaceKeyword(SyntaxFactory
+					.Token(SyntaxFactory
+						.TriviaList(SyntaxFactory
+							.CarriageReturnLineFeed, SyntaxFactory
+							.CarriageReturnLineFeed), 
+						SyntaxKind.NamespaceKeyword, SyntaxFactory
+						.TriviaList(SyntaxFactory
+							.Space)))
+				.WithOpenBraceToken(SyntaxFactory
+					.Token(SyntaxFactory
+						.TriviaList(), 
+						SyntaxKind.OpenBraceToken, SyntaxFactory
+						.TriviaList(SyntaxFactory
+							.CarriageReturnLineFeed)))
+				.WithCloseBraceToken(SyntaxFactory
+					.Token(SyntaxFactory
+						.TriviaList(SyntaxFactory
+							.CarriageReturnLineFeed),
+						SyntaxKind.CloseBraceToken, SyntaxFactory
+						.TriviaList()))
+				.AddMembers(SyntaxFactory
+					.ClassDeclaration(SyntaxFactory
+						.Identifier(SyntaxFactory
+							.TriviaList(),
+							classDeclaration.Identifier.Text + "Test", SyntaxFactory
+							.TriviaList(SyntaxFactory
+								.CarriageReturnLineFeed)))
+					.WithAttributeLists(SyntaxFactory
+						.SingletonList(SyntaxFactory
+							.AttributeList(SyntaxFactory
+								.SingletonSeparatedList(SyntaxFactory
+									.Attribute(SyntaxFactory
+										.IdentifierName("TestFixture"))))
+							.WithOpenBracketToken(SyntaxFactory
+								.Token(SyntaxFactory
+									.TriviaList(SyntaxFactory
+										.CarriageReturnLineFeed, SyntaxFactory
+										.Whitespace(Tab)),
+									SyntaxKind.OpenBracketToken, SyntaxFactory
+									.TriviaList()))
+						.WithCloseBracketToken(SyntaxFactory
+							.Token(SyntaxFactory
+								.TriviaList(),
+								SyntaxKind.CloseBracketToken, SyntaxFactory
+								.TriviaList(SyntaxFactory
+									.CarriageReturnLineFeed)))))
+					.WithModifiers(SyntaxFactory
+						.TokenList(SyntaxFactory
+							.Token(SyntaxFactory
+								.TriviaList(SyntaxFactory
+									.Whitespace(Tab)), 
+								SyntaxKind.PublicKeyword, SyntaxFactory
+								.TriviaList(SyntaxFactory.Space))))
+					.WithKeyword(SyntaxFactory
+						.Token(SyntaxFactory
+							.TriviaList(),
+							SyntaxKind.ClassKeyword, SyntaxFactory
+							.TriviaList(SyntaxFactory
+								.Space)))
+					.WithOpenBraceToken(SyntaxFactory
+						.Token(SyntaxFactory
+							.TriviaList(SyntaxFactory
+								.Whitespace(Tab)),
+							SyntaxKind.OpenBraceToken, SyntaxFactory
+							.TriviaList(SyntaxFactory
+								.CarriageReturnLineFeed)))
+					.WithCloseBraceToken(SyntaxFactory
+						.Token(SyntaxFactory
+							.TriviaList(SyntaxFactory
+								.Whitespace(Tab)),
+							SyntaxKind.CloseBraceToken, SyntaxFactory
+							.TriviaList()))
 					.AddMembers(AddTestMethods(classDeclaration)));
 
 			return unit.NormalizeWhitespace().AddMembers(@namespace).ToFullString();
@@ -105,46 +150,91 @@ namespace Core
 				.Where(method => method.Modifiers.Any(SyntaxKind.PublicKeyword));
 
 			return methods.Select(method => SyntaxFactory
-				.MethodDeclaration(
-					SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxFactory.TriviaList(), SyntaxKind.VoidKeyword,
-						SyntaxFactory.TriviaList(SyntaxFactory.Space))),
-					SyntaxFactory.Identifier(method.Identifier.Text + "Test"))
-				.WithAttributeLists(SyntaxFactory.SingletonList(SyntaxFactory
-					.AttributeList(
-						SyntaxFactory.SingletonSeparatedList(
-							SyntaxFactory.Attribute(SyntaxFactory.IdentifierName("Test"))))
-					.WithOpenBracketToken(SyntaxFactory.Token(
-						SyntaxFactory.TriviaList(SyntaxFactory.Whitespace(Tab + Tab)),
-						SyntaxKind.OpenBracketToken, SyntaxFactory.TriviaList()))
-					.WithCloseBracketToken(SyntaxFactory.Token(SyntaxFactory.TriviaList(), SyntaxKind.CloseBracketToken,
-						SyntaxFactory.TriviaList(SyntaxFactory.LineFeed)))))
-				.WithModifiers(SyntaxFactory.TokenList(SyntaxFactory.Token(
-					SyntaxFactory.TriviaList(SyntaxFactory.Whitespace(Tab + Tab)),
-					SyntaxKind.PublicKeyword, SyntaxFactory.TriviaList(SyntaxFactory.Space))))
-				.WithParameterList(SyntaxFactory.ParameterList().WithCloseParenToken(
-					SyntaxFactory.Token(SyntaxFactory.TriviaList(), SyntaxKind.CloseParenToken,
-						SyntaxFactory.TriviaList(SyntaxFactory.LineFeed))))
-				.WithBody(SyntaxFactory.Block()
-					.WithOpenBraceToken(SyntaxFactory.Token(
-						SyntaxFactory.TriviaList(SyntaxFactory.Whitespace(Tab + Tab)),
-						SyntaxKind.OpenBraceToken, SyntaxFactory.TriviaList(SyntaxFactory.LineFeed)))
-					.WithCloseBraceToken(SyntaxFactory.Token(
-						SyntaxFactory.TriviaList(SyntaxFactory.Whitespace(Tab + Tab)),
-						SyntaxKind.CloseBraceToken,
-						SyntaxFactory.TriviaList(SyntaxFactory.LineFeed, SyntaxFactory.Whitespace(""),
-							SyntaxFactory.LineFeed)))).AddBodyStatements(SyntaxFactory
+				.MethodDeclaration(SyntaxFactory
+					.PredefinedType(SyntaxFactory
+						.Token(SyntaxFactory
+							.TriviaList(),
+							SyntaxKind.VoidKeyword, SyntaxFactory
+							.TriviaList(SyntaxFactory
+							.Space))), SyntaxFactory
+					.Identifier(method.Identifier.Text + "Test"))
+				.WithAttributeLists(SyntaxFactory
+					.SingletonList(SyntaxFactory
+						.AttributeList(SyntaxFactory
+							.SingletonSeparatedList(SyntaxFactory
+								.Attribute(SyntaxFactory
+									.IdentifierName("Test"))))
+						.WithOpenBracketToken(SyntaxFactory
+							.Token(SyntaxFactory
+								.TriviaList(SyntaxFactory
+									.Whitespace(Tab + Tab)),
+								SyntaxKind.OpenBracketToken, SyntaxFactory
+								.TriviaList()))
+						.WithCloseBracketToken(SyntaxFactory
+							.Token(SyntaxFactory
+								.TriviaList(), 
+								SyntaxKind.CloseBracketToken, SyntaxFactory
+								.TriviaList(SyntaxFactory
+								.LineFeed)))))
+				.WithModifiers(SyntaxFactory
+					.TokenList(SyntaxFactory
+						.Token(SyntaxFactory
+							.TriviaList(SyntaxFactory
+								.Whitespace(Tab + Tab)),
+							SyntaxKind.PublicKeyword, SyntaxFactory
+							.TriviaList(SyntaxFactory
+							.Space))))
+				.WithParameterList(SyntaxFactory
+					.ParameterList()
+					.WithCloseParenToken(SyntaxFactory
+					.Token(SyntaxFactory
+						.TriviaList(), 
+						SyntaxKind.CloseParenToken, SyntaxFactory
+						.TriviaList(SyntaxFactory
+						.LineFeed))))
+				.WithBody(SyntaxFactory
+					.Block()
+					.WithOpenBraceToken(SyntaxFactory
+						.Token(SyntaxFactory
+							.TriviaList(SyntaxFactory
+								.Whitespace(Tab + Tab)),
+							SyntaxKind.OpenBraceToken, SyntaxFactory
+							.TriviaList(SyntaxFactory
+								.LineFeed)))
+					.WithCloseBraceToken(SyntaxFactory
+						.Token(SyntaxFactory
+							.TriviaList(SyntaxFactory
+								.Whitespace(Tab + Tab)),
+							SyntaxKind.CloseBraceToken,	SyntaxFactory
+							.TriviaList(SyntaxFactory
+								.LineFeed, SyntaxFactory
+								.LineFeed))))
+				.AddBodyStatements(SyntaxFactory
 					.ExpressionStatement(SyntaxFactory
-						.InvocationExpression(SyntaxFactory.MemberAccessExpression(
-							SyntaxKind.SimpleMemberAccessExpression,
-							SyntaxFactory.IdentifierName(SyntaxFactory.Identifier(
-								SyntaxFactory.TriviaList(
-									SyntaxFactory.Whitespace(Tab + Tab + Tab)), "Assert",
-								SyntaxFactory.TriviaList())), SyntaxFactory.IdentifierName("Fail"))).WithArgumentList(
-							SyntaxFactory.ArgumentList(SyntaxFactory.SingletonSeparatedList(
-								SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(
-									SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal("autogeneratedtest")))))))
-					.WithSemicolonToken(SyntaxFactory.Token(SyntaxFactory.TriviaList(), SyntaxKind.SemicolonToken,
-						SyntaxFactory.TriviaList(SyntaxFactory.LineFeed))))).Cast<MemberDeclarationSyntax>().ToArray();
+						.InvocationExpression(SyntaxFactory
+							.MemberAccessExpression(
+								SyntaxKind.SimpleMemberAccessExpression, SyntaxFactory
+								.IdentifierName(SyntaxFactory
+									.Identifier(SyntaxFactory
+										.TriviaList(SyntaxFactory
+											.Whitespace(Tab + Tab + Tab)),
+										"Assert", SyntaxFactory
+										.TriviaList())), SyntaxFactory
+								.IdentifierName("Fail")))
+						.WithArgumentList(SyntaxFactory
+							.ArgumentList(SyntaxFactory
+								.SingletonSeparatedList(SyntaxFactory
+									.Argument(SyntaxFactory
+										.LiteralExpression(
+											SyntaxKind.StringLiteralExpression, SyntaxFactory
+											.Literal("autogeneratedtest")))))))
+					.WithSemicolonToken(SyntaxFactory
+						.Token(SyntaxFactory
+							.TriviaList(), 
+							SyntaxKind.SemicolonToken, SyntaxFactory
+							.TriviaList(SyntaxFactory
+								.LineFeed)))))
+			.Cast<MemberDeclarationSyntax>().ToArray();
 		}
 
 	}
